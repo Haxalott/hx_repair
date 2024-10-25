2d57af621f0aefab93bde142134e2be963ada37a
Haxalott's Developments <43390732+Haxalott@users.noreply.github.com>
2024-10-25 15:00:22 +1100

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,8 +1,13 @@
 Config = {}
 
 Config.CarWashEnabled = true
-Config.RepairKitEnabled = true
-Config.RepairBayEnabled = false
+Config.RepairKitEnabled = false
+Config.RepairBayEnabled = true
+
+Config.EnableVehicleHealthInPayment = {
+    engine = true,
+    body = true
+}
 
 Config.CarWashAnims = {
     dict = 'amb@world_human_maid_clean@base',
@@ -19,5 +24,8 @@ Config.ProgressType = 'circle' -- circle / bar
 
 Config.RepairBays = {
     {location = vec3(449.48, -972.62, 25.71), name = 'Repair Bay', sprite = 402, blipColour = 0, price = 5000, range = 5, blip = true}, -- blip == blip enabled? -- name == the blip name -- blip == the blip (found at https://docs.fivem.net/docs/game-references/blips/) -- blipColour == Find at the same place as the blip icon -- Price == the price it costts
-    {location = vec3(425.69, -1021.96, 28.91), name = 'Repair Bay2', sprite = 402, blipColour = 0, price = 6000, range = 5, blip = true}
+}
+
+Config.CarWashes = {
+    {location = vec3(23.62, -1391.97, 29.33), name = 'Car Wash', sprite = 100, blipColour = 0, price = 2000, range = 5, blip = true}
 }
